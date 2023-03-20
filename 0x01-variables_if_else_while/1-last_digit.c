af82be6e19ab4bb6e6aafe43cca20c1d01294292
Ek-04 <wkariuki.e@gmail.com>
2023-03-20 08:22:16 -0700

    task 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,8 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
+ * main - determins if a number is positive , negative or zero
  *
  * Return: Always 0 (Success)
  */

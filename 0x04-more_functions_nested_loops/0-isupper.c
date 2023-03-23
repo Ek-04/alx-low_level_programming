0eef009e22b88ddb52570008dfe486c6b06ae45c
Ek-04 <wkariuki.e@gmail.com>
2023-03-23 08:23:39 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+#include"main.h"
+/*
+ *_isdigit -  checks for a digit (0 through 9).
+ *Retuurn: i if number is a digit otherwise
+ *Return:0
+ *@c: is number to be checked
+ *
+ */
+int _isdigit(int c)
+{
+int c;
+int i;
+
+while (i <= 9 || i <= 0)
+{
+if (c == i)
+{
+return (i);
+}
+else
+{
+return (0);
+}
+}
+}

2ec49ee40245acf8a71133b95232c110fe76b5b9
Ek-04 <wkariuki.e@gmail.com>
2023-04-03 23:16:03 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,18 @@
 #include<string.h>
 /**
- *
- *
- *
- *
+ **_memcpy - copies memory area
+ *@dest: content to be cpied into
+ *@src: cntent to be copied
+ *Return: (0)
+ *@n: size of the count
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (0);
+}

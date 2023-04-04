97259f993bf5dca3ba6e1a28b79ed58cd4d9e68c
Ek-04 <wkariuki.e@gmail.com>
2023-04-04 11:35:43 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include<string.h>
+/**
+ **_strpbrk - searches string s for any set of bytes of characters in accept
+ *@s: string to be checked
+ *@accept: string containing the characers allowed in prefix of s
+ *Return: a pointer to the byte in s if accept character s found and returns NULL if not
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		for (j = 0; j != '\0'; j++)
+		{
+		if (s[i] == accept[j])
+			return (s + i);
+		}
+		}
+return (NULL);
+}

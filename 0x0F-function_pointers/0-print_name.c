f5f597a05af464a65606ecc993c1fa6490cf72b7
Ek-04 <wkariuki.e@gmail.com>
2023-04-19 09:20:16 -0700

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,14 +5,19 @@
  *print_name - entry
  *@name: ch
  *@f: pointer
- *
+ *Return: 0
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL || f != NULL)
+	if (name == NULL || f == NULL)
 	{
 		return;
 	}
 	f(name);
+}
+
+int main(void)
+{
+return (0);
 }
